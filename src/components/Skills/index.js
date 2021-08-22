a6bace8135f0080211ba422e89f386d7c239c337
ALU STUDENT 398 <tnsanzabarinda17@alustudent.com>
2021-08-22 23:36:07 +0200

    addedfile

---
+++ src/components/Skills/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../LandingPage/style.css'
+function index() {
+    return (
+        <div className="skills">
+                    <div className="skill-card">
+                        <h1>Theoneste Nsanzabarinda</h1>
+                        <div>
+                            <h4>Skills</h4>
+                            <ol>
+                                <li>HTML5</li>
+                                <li>CSS3</li>
+                                <li>Javascript</li>
+                                <li>React.JS</li>
+                                <li>Redux</li>
+                                <li>Github</li>
+                                <li>Java && Python</li>
+                                <li>c && c++</li>
+                            </ol>
+                            <span>21 August 2021</span>
+                        </div>
+                    </div>
+                </div>
+    )
+}
+
+export default index
